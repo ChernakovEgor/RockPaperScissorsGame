196f1416b9a315fcd7452bdb1f6df2b895fceb3a
Egor Chernakov <chernakov.eg@gmail.com>
2021-02-19 17:56:34 +0300

    Small UI Changes

--- RockPaperScissorsGame/ContentView.swift
+++ RockPaperScissorsGame/ContentView.swift
@@ -28,39 +28,45 @@ struct ContentView: View {
     @State private var questionNumber = 1
     
     var body: some View {
-        VStack(spacing: 30)  {
-            Text("Your Score: \(score)")
-            HStack {
-                Text("\(shouldWin ? "WIN" : "LOSE")")
-                Text("against")
-                Text("\(moves[computerMove].capitalized)")
-            }
-            HStack {
-                ForEach(0..<3) { move in
-                    Button(action: {
-                        let playerHasWon = hasPlayerWon(withMove: moves[move], against: moves[computerMove])
-                        if shouldWin {
-                            score += playerHasWon ? 1 : -1
-                        } else {
-                            score += playerHasWon ? -1 : 1
+        ZStack {
+            AngularGradient(gradient: Gradient(colors: [.black, .blue, .black, .gray, .gray]), center: .top)
+                .opacity(0.7)
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30)  {
+                Text("Your Score: \(score)")
+                HStack {
+                    Text("\(shouldWin ? "WIN" : "LOSE")")
+                    Text("against")
+                    Text("\(moves[computerMove].uppercased())")
+                }.font(Font.headline.weight(.heavy))
+                HStack {
+                    ForEach(0..<3) { move in
+                        Button(action: {
+                            let playerHasWon = hasPlayerWon(withMove: moves[move], against: moves[computerMove])
+                            if shouldWin {
+                                score += playerHasWon ? 1 : -1
+                            } else {
+                                score += playerHasWon ? -1 : 1
+                            }
+                            computerMove = Int.random(in: 0...2)
+                            shouldWin = Bool.random()
+                            questionNumber += 1
+                            if questionNumber == 11 {
+                                gameIsOver = true
+                            }
+                        }) {
+                            CardView(name: "\(moves[move])")
                         }
-                        computerMove = Int.random(in: 0...2)
-                        shouldWin = Bool.random()
-                        questionNumber += 1
-                        if questionNumber == 11 {
-                            gameIsOver = true
-                        }
-                    }) {
-                        CardView(name: "\(moves[move])")
                     }
                 }
+                Spacer()
             }
-            Spacer()
-        }.alert(isPresented: $gameIsOver) {
-            Alert(title: Text("Game Over"), message: Text("Your Score Is \(score)"), dismissButton: .default(Text("Play Again")) {
-                    score = 0
-                    questionNumber = 1
-            })
+            .alert(isPresented: $gameIsOver) {
+                Alert(title: Text("Game Over"), message: Text("Your Score Is \(score)"), dismissButton: .default(Text("Play Again")) {
+                        score = 0
+                        questionNumber = 1
+                })
+            }
         }
     }
     
