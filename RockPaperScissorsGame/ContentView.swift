4650f67f7564ec3249ca1c7d08deeca6fd7ffacc
Egor Chernakov <chernakov.eg@gmail.com>
2021-02-19 17:34:08 +0300

    Basic Logic Completed

--- RockPaperScissorsGame/ContentView.swift
+++ RockPaperScissorsGame/ContentView.swift
@@ -7,10 +7,66 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CardView: View {
+    let name: String
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Image("\(name)")
+            .resizable()
+            .frame(width: 100, height: 150, alignment: .center)
+            .cornerRadius(7)
+    }
+}
+
+struct ContentView: View {
+    
+    let moves = ["rock", "paper", "scissors"]
+    
+    @State private var computerMove = Int.random(in: 0...2)
+    @State private var shouldWin = Bool.random()
+    @State private var score = 0
+    @State private var gameIsOver = false
+    @State private var questionNumber = 1
+    
+    var body: some View {
+        VStack(spacing: 30)  {
+            Text("Your Score: \(score)")
+            HStack {
+                Text("\(shouldWin ? "WIN" : "LOSE")")
+                Text("against")
+                Text("\(moves[computerMove].capitalized)")
+            }
+            HStack {
+                ForEach(0..<3) { move in
+                    Button(action: {
+                        let playerHasWon = hasPlayerWon(withMove: moves[move], against: moves[computerMove])
+                        if shouldWin {
+                            score += playerHasWon ? 1 : -1
+                        } else {
+                            score += playerHasWon ? -1 : 1
+                        }
+                        computerMove = Int.random(in: 0...2)
+                        shouldWin = Bool.random()
+                        questionNumber += 1
+                        if questionNumber == 11 {
+                            gameIsOver = true
+                        }
+                    }) {
+                        CardView(name: "\(moves[move])")
+                    }
+                }
+            }
+            Spacer()
+        }.alert(isPresented: $gameIsOver) {
+            Alert(title: Text("Game Over"), message: Text("Your Score Is \(score)"), dismissButton: .default(Text("Play Again")) {
+                    score = 0
+                    questionNumber = 1
+            })
+        }
+    }
+    
+    func hasPlayerWon(withMove: String, against move: String) -> Bool {
+        let winScenarios = ["rockscissors", "scissorspaper", "paperrock"]
+        return winScenarios.contains("\(withMove)\(move)")
     }
 }
 
